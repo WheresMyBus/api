635a5f1ee75b1c14ce2d2784a01baff2a47c159a
Daniel Houtsma <danhoutsma@gmail.com>
2016-11-01 10:50:52 -0700

    Disable test failure

--- test/integration/failure_test.rb
+++ test/integration/failure_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 
 class FailureTest < Minitest::Test
-  def test_fails_when_expected_on_travis
-    assert 0 == nil
-  end
+  # def test_fails_when_expected_on_travis
+  #   assert 0 == nil
+  # end
 end
