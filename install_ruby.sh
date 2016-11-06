bdde28784d157c1a56e112a0bf69cb67d71bc2ef
nbissiri <nbissiri@localhost.localdomain>
2016-11-06 12:15:08 -0800

    changed install_ruby.sh to be closer to working

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,4 @@
-echo "installing Ruby and Rails"
+echo "installing Ruby, Rails, Postgres, Heroku local"
 sudo yum install gcc-c++ patch readline readline-devel zlib zlib-devel
 sudo yum install libyaml-devel libffi-devel openssl-devel make
 sudo yum install bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
@@ -7,9 +7,11 @@ sudo curl -L get.rvm.io | sudo bash -s stable --auto-dotfiles
 source /etc/profile.d/rvm.sh
 rvm reload
 rvm requirements run
-sudo rvm install 2.2.4
-rvm use 2.2.4 --default
+sudo rvm install 2.3.1
+rvm use 2.3.1 --default
+sudo gem install mysql -- --with-mysql-config=/usr/local/mysql/bin/mysql_config
 sudo gem install rails
-sudo gem install rake -v '11.3.0'
-sudo bundle update sqlite3
+sudo gem install rake
+sudo yum install postgresql-devel
+bundle install
 
