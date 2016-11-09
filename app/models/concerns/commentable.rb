bcc88a9475ee0ebef203d86bdef20d1d7fe6a43c
Daniel Houtsma <danhoutsma@gmail.com>
2016-11-09 13:56:49 -0800

    Add convenience methods for voting and commenting

--- app/models/concerns/commentable.rb
+++ app/models/concerns/commentable.rb
@@ -4,4 +4,8 @@ module Commentable
   included do
     has_many :comments, as: :commentable
   end
+
+  def comment!(message, user_id = nil)
+    comments.create message: message, user_id: user_id
+  end
 end
--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -4,4 +4,20 @@ module Votable
   included do
     has_many :votes, as: :votable
   end
+
+  def upvotes
+    votes.up
+  end
+
+  def downvotes
+    votes.down
+  end
+
+  def upvote!(user_id = nil)
+    upvotes.create user_id: user_id
+  end
+
+  def downvote!(user_id = nil)
+    downvotes.create user_id: user_id
+  end
 end
