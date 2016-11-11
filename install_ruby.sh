e44094c7758d06a5fd6f213892beeafdeb2e6598
nbissiri <nbissiri@localhost.localdomain>
2016-11-11 03:23:00 -0800

    install_ruby.sh now completes dependancies for heroku local

--- install_ruby.sh
+++ install_ruby.sh
@@ -12,5 +12,22 @@ rvm use 2.3.1 --default
 sudo gem install rails
 sudo gem install rake
 sudo yum install postgresql-devel
+
+#What follows are manual installations of dependancies that break bundler for unknown reasons
+sudo gem install minitest -v '5.9.1'
+sudo gem install jsonapi-parser -v '0.1.1.beta3'
+sudo gem install jsonapi-renderer -v '0.1.1.beta1'
+sudo gem install multi_xml -v '0.5.5'
+sudo gem install pg -v '0.19.0'
+sudo gem install puma -v '3.6.0'
+sudo gem install jsonapi -v '0.1.1.beta6'
+sudo gem install httparty -v '0.14.0'
+sudo gem install factory_girl -v '4.7.0'
+sudo gem install active_model_serializers -v '0.10.2'
+sudo gem install factory_girl_rails -v '4.7.0'
+
+#install everything else with bundler
 bundle install
 
+#install heroku CLI
+sudo wget -q0- https://toolbelt.heroku.com/install.sh | sh
