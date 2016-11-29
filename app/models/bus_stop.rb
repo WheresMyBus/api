30915dad58faa7f7b9dd0ee871f49450da7a2702
Daniel Houtsma <danhoutsma@gmail.com>
2016-11-28 22:17:53 -0800

    Add more documentation to models

--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -1,34 +1,42 @@
+# Models a bus stop.
 class BusStop
   include ActiveModel::Serialization
 
+  # generates getter methods for the specified attributes
   attr_reader :id,
               :location,
               :direction,
               :name,
               :routes
 
+  # Constructs a new object from a OneBusAway bus stop object and a list of routes.
   def initialize(bus_stop, routes)
-    @id = bus_stop.id
-    @location = bus_stop.location
-    @direction = bus_stop.direction
-    @name = bus_stop.name
+    @id = bus_stop&.id
+    @location = bus_stop&.location
+    @direction = bus_stop&.direction
+    @name = bus_stop&.name
     @routes = routes
   end
 
+  # Gets all bus stops near the given location. A search radius (in meters) can also be specified.
   def self.for_location(location, radius: nil)
+    # gets the bus stop information from OneBusAway, including the referenced routes
     stops, routes = OneBusAway.stops_for_location(location,
                                                   radius: radius,
                                                   include_routes: true)
 
     stops.map do |stop|
+      # gets the list of routes that corresponds to the bus stop object's list of route ids
       filtered_routes = routes.select do |route|
         stop.route_ids.include? route.id
       end
 
+      # creates a list of routes from the list of OneBusAway route objects
       filtered_routes.map! do |route|
         Route.new route
       end
 
+      # creates a new bus stop object from the OneBusAway bus stop object and list of routes
       new stop, filtered_routes
     end
   end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,27 +1,34 @@
+# Models a bus route.
 class Route
+
+  # generates getter methods for the specified attributes
   attr_reader :id,
               :number,
               :name
 
+  # Constructs a new object from a OneBusAway route object.
   def initialize(route)
-    @id = route.id
-    @number = route.short_name.presence
-    @name = route.description.presence ||
-            route.long_name.presence
+    @id = route&.id
+    @number = route&.short_name.presence
+    @name = route&.description.presence ||
+            route&.long_name.presence
   end
 
+  # Gets all routes for the supported transit agencies. The results are cached for one week in
+  # order to limit the number of calls to the OneBusAway API.
   def self.all
     Rails.cache.fetch 'routes', expires_in: 1.week do
       SUPPORTED_AGENCIES.map do |id|
-        OneBusAway.routes_for_agency(id).map do |route|
+        OneBusAway.routes_for_agency(id).map do |route| # gets routes from OneBusAway
           new route
-        end.reject do |route|
+        end.reject do |route| # filters out the bus routes with no route number
           route.number.nil?
         end
       end.flatten
     end
   end
 
+  # Gets all routes where the given query matches either a route's name or number, ignoring case.
   def self.filter(query)
     query = query.to_s.downcase
 
@@ -34,41 +41,52 @@ class Route
     end
   end
 
+  # Gets the route corresponding to the given id. The result is cached for one week in order to
+  # limit the number of calls to the OneBusAway API.
   def self.find(id)
     Rails.cache.fetch 'routes/#{id}', expires_in: 1.week do
       new OneBusAway.route(id)
     end
   end
 
+  # Gets all of the alerts belonging to the route.
   def alerts
     RouteAlert.where route_id: id
   end
 
+  # Gets the current latitude, longitude, and direction for each bus on the route.
   def bus_locations
     OneBusAway.trips_for_route(id, include_status: true).map do |trip|
       {
-        lat: trip.status.position.lat,
-        lon: trip.status.position.lon,
-        direction: degrees_to_direction(trip.status.orientation)
+        lat: trip&.status&.position&.lat,
+        lon: trip&.status&.position&.lon,
+        direction: degrees_to_direction(trip&.status&.orientation)
       }
     end
   end
 
   private
 
-  # 1   - Metro Transit
-  # 3   - Pierce Transit
-  # 19  - Intercity Transit
-  # 23  - City of Seattle
-  # 29  - Community Transit
-  # 40  - Sound Transit
-  # 95  - Washington State Ferries
-  # 97  - Everett Transit
-  # 98  - Seattle Children's Hospital Shuttle
-  # 99  - GO Transit
-  # KMD - Kingcounty Marine Division
+  # All transit agencies supported by OneBusAway:
+  #
+  #   ID  | NAME
+  #   ----+------------------------------------
+  #   1   | Metro Transit
+  #   3   | Pierce Transit
+  #   19  | Intercity Transit
+  #   23  | City of Seattle
+  #   29  | Community Transit
+  #   40  | Sound Transit
+  #   95  | Washington State Ferries
+  #   97  | Everett Transit
+  #   98  | Seattle Children's Hospital Shuttle
+  #   99  | GO Transit
+  #   KMD | Kingcounty Marine Division
+
+  # list of transit agencies to get route information about
   SUPPORTED_AGENCIES = %w(1 29 40).freeze
 
+  # Translates a number of degrees into the corresponding cardinal direction.
   def degrees_to_direction(degrees)
     %i(N NW W SW S SE E NE)[((degrees / 45.0) + 0.5).floor % 8]
   end
